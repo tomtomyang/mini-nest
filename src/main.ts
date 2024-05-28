da269103d2ca699d7efd8f13859e4a6969558ab3
tomtomyang <tomtomyang@tencent.com>
2024-05-28 11:35:40 +0800

    feat: README.md

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = new Application(AppModule);
-  app.listen(3000, () => console.log('app is listening on port 3000'));
+  app.listen(3000, () => console.log('App is listening on port 3000'));
 }
 
 bootstrap();
